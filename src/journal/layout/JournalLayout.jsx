b56b4141942a5216864a26a0a8178120e3686902
JuanCM <juancampuzano2356@gmail.com>
2024-05-10 15:25:47 -0500

    Componente SideBar Agregado

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,7 +1,7 @@
-import { Box } from "@mui/material"
-import { NavBar } from "../components";
+import { Box, Toolbar } from "@mui/material"
+import { NavBar, SideBar } from "../components";
 
-const drawerWidth = 240; 
+const drawerWidth = 280; 
 
 
 export const JournalLayout = ({ children }) => {
@@ -9,14 +9,16 @@ export const JournalLayout = ({ children }) => {
     <Box sx={{ display: 'flex' }}>
 
     <NavBar drawerWidth={ drawerWidth } />
-    
-    {/* SideBar */}
+
+    <SideBar drawerWidth={ drawerWidth } />    
 
     <Box
         component='main'
         sx={{ flexGrow: 1, p: 3 }}
     >
-        {/* Toolbar */}
+
+        <Toolbar />
+
         { children }
     </Box>
 
