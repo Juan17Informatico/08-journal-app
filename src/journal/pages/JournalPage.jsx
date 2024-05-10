bf7b26720cbdd4042f197a565ed243fb99f5335c
Juan17Informatico <juancampuzano2356@gmail.com>
2024-05-09 22:33:00 -0500

    Add layout/JournalLayout and modified JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,18 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">Journal Page</Typography>
-    </>
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure quae, voluptatem vel iusto nihil nobis aliquid ullam aut, labore molestiae esse alias quidem, deleniti amet facere. Maiores, unde sit? 
+      </Typography>
+
+      {/* NothingSelected */}
+      {/* NoteView */}
+
+    </JournalLayout>
   )
 }
