42f5737923758e742f279cdf08900bc7abdf9c0d
JuanCM <juancampuzano2356@gmail.com>
2024-07-30 16:32:54 -0500

    Se finaliza la clase 340

--- tests/auth/pages/LoginPage.test.jsx
+++ tests/auth/pages/LoginPage.test.jsx
@@ -1,14 +1,25 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { LoginPage } from "../../../src/auth/pages";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "../../../src/store/auth";
+import { startGoogleSignIn } from "../../../src/store/auth/thunks";
 import { MemoryRouter } from "react-router-dom";
+import { notAuthenticatedState } from "../../fixtures/authFixtures";
+
+const mockStartGoogleSignIn = jest.fn();
+
+jest.mock('../../../src/store/auth/thunks', () => ({
+    startGoogleSignIn: () => mockStartGoogleSignIn
+}));
 
 const store = configureStore({
     reducer: {
         auth: authSlice.reducer
     },
+    preloadedState: {
+        auth: notAuthenticatedState
+    }
 });
 
 describe('Pruebas en <LoginPage />', () => {
@@ -26,5 +37,23 @@ describe('Pruebas en <LoginPage />', () => {
         expect( screen.getAllByText('Login').length ).toBeGreaterThan(1);
 
     }); 
+
+    test('debe de llamar el startGoogleSignIn', () => {
+        
+        render(
+            <Provider store={store}>
+                <MemoryRouter>
+                    <LoginPage />
+                </MemoryRouter>
+            </Provider>
+        );
+
+        const btnGoogle = screen.getByLabelText('google-btn'); 
+        fireEvent.click(btnGoogle); 
+
+
+        expect(mockStartGoogleSignIn).toHaveBeenCalled();
+
+    });
 
 });
